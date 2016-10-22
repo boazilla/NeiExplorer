79481c50c663c0727ac941e0e4a20ad8fcd432e0
Boaz Vishkel <boazvishkel@gmail.com>
2016-10-22 23:14:19 +0300

    upto pause in development

---
+++ app/src/main/java/javaghost/neiexplorer/FetchPlacesIntentService.java
@@ -0,0 +1,199 @@
+package javaghost.neiexplorer;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class FetchPlacesIntentService extends IntentService {
+   public static final String
+           ACTION_20          = "javaghost.neiexplorer.action.20",
+//           yell_LOCATION      = "yell_location",
+//           yell_RADIUS        = "yell_radius",
+//           yell_WORD          = "yell_word",
+//           yell_TYPE          = "yell_type",
+           debug_title        = "FetchPlaces ** ";
+   static final String
+           st_ZERO_RESULTS    = "ZERO_RESULTS",
+           st_REQUEST_DENIED  = "REQUEST_DENIED",
+           st_OVER_QUERY_LIMIT= "OVER_QUERY_LIMIT",
+           st_INVALID_REQUEST = "INVALID_REQUEST",
+           st_OK              = "OK";
+   ArrayList<JustPlace> all_places;
+   StatePillar reflection = StatePillar.getInstance();
+
+   public FetchPlacesIntentService() {
+      super("FetchPlacesIntentService");
+   }
+
+   @Override
+   protected void onHandleIntent(Intent intent) {
+      if (intent != null) {
+         final String action = intent.getAction();
+         final Bundle params = intent.getExtras();
+         if (ACTION_20.equals(action) && doFull20Search(reflection.getCenterNow(),
+                 reflection.getLookupRadius(), reflection.getWordOfBlake(), reflection.getTypeWriter()))
+         {
+            // implicit call of FullSearch() returned TRUE;
+            pushPreference( String.valueOf(all_places.size()));
+            reflection.DAO.storeSynth(all_places);
+         }
+      }
+   }
+
+   void pushPreference(String message_me){
+      reflection.shE.putString(getString(R.string.key_srvc_flag), message_me);
+      reflection.shE.commit();
+   }
+
+   boolean doFull20Search(String location_, String radius_, String word_, String type_) {
+      // https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=
+      // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&type=store&radius=5000&key=
+      // https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken= &key=
+      String
+              DEFAULT_SEARCH_WHAT = "place_of_interest",
+              API_K = getString(R.string.api_K_web),
+              URL_ROOT = "https://maps.googleapis.com/maps/api/place/",
+              _FULL_SEARCH_WT     = "nearbysearch/json?location=%2$s&radius=%3$s&keyword=%4$s&type=%5$s&key=%1$s",
+              _FULL_SEARCH_W      = "nearbysearch/json?location=%2$s&radius=%3$s&keyword=%4$s&key=%1$s",
+              _FULL_SEARCH_T      = "nearbysearch/json?location=%2$s&radius=%3$s&type=%4$s&key=%1$s",
+              _FULL_SEARCH_D      = "nearbysearch/json?rankby=distance&location=%2$s&radius=%3$s&keyword=%4$s&type=%5$s&key=%1$s",
+              _FULL_SEARCH_DN     = "nearbysearch/json?location=%2$s&radius=%3$s&type=place_of_interest&key=%1$s",
+              _DETAILED_SEARCH    = "details/json?placeid=%2$s&key=%1$s",
+              GET_PAGE            = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=%2$s&key=%1$s";
+
+      String try_url = "";
+
+      if (reflection._by_keys && reflection._by_word)
+         try_url = URL_ROOT + String.format(_FULL_SEARCH_WT, API_K, location_, radius_, word_, type_);
+      else if (reflection._by_word)
+         try_url = URL_ROOT + String.format(_FULL_SEARCH_W, API_K, location_, radius_, word_);
+      else if (reflection._by_keys)
+         try_url = URL_ROOT + String.format(_FULL_SEARCH_T, API_K, location_, radius_, type_);
+      else if(!(reflection._by_keys && reflection._by_word))
+         try_url = URL_ROOT + String.format(_FULL_SEARCH_DN, API_K, location_, radius_);
+      pushPreference(try_url);
+      String next_token = "next_page_token";
+      all_places = new ArrayList<>();
+      int loop = 0, passes_left =3, continued = 3;
+ask_url:
+      do {
+         String assembled_str = readJStringFromFarFarAway(try_url);
+         if (assembled_str.length() < 10) // i.e. 'false'
+         {
+            Log.d(debug_title, "reply on POST: " +assembled_str);
+            return false; // Aborted
+         }
+
+         JSONObject bootObj = null;
+         JSONArray place_cluster = null;
+         try {
+            bootObj = new JSONObject(assembled_str);
+            String status = bootObj.getString("status");
+            switch (status) {
+               case st_REQUEST_DENIED:
+               case st_OVER_QUERY_LIMIT:
+                  pushPreference(status);
+                  return false;
+               case st_INVALID_REQUEST:
+                  pushPreference("delayed "+ (4-continued) + " red: " + all_places.size());
+                  if (all_places.size()>0) {
+                     SystemClock.sleep(2000);
+                     if(continued-- >0)
+                        continue ask_url;
+                     else
+                        return true;
+                  }
+               case st_ZERO_RESULTS:
+                  pushPreference(status);
+                  return true;
+               case st_OK:
+                  break;
+            }
+            if (bootObj.has("next_page_token") && !bootObj.getString("next_page_token").isEmpty()) {
+               String old_token = next_token;
+               next_token = bootObj.getString("next_page_token");
+               if (old_token.equals(next_token)) {
+                  passes_left = 0;
+                  next_token = "no_pages";
+               }
+            }
+            else {
+               passes_left = 0;
+               next_token="no_pages";
+            }
+            place_cluster = bootObj.getJSONArray("results");
+         } catch (JSONException e) {
+            e.printStackTrace();
+            pushPreference(e.getLocalizedMessage());
+            return false;
+         }
+         pushPreference(next_token);
+         loop = 0;
+         assert place_cluster != null;
+         while (loop < 20 && !place_cluster.isNull(loop)) {
+            try {
+               JSONObject next = place_cluster.getJSONObject(loop);
+               all_places.add(new JustPlace(next));
+            } catch (JSONException e) {
+               e.printStackTrace();
+               passes_left = 0;
+            }
+            loop++;
+         }
+         pushPreference(String.valueOf(all_places.size()));
+         passes_left--;
+         if (next_token.length() > 15 && passes_left > 0)
+            try_url = String.format(GET_PAGE, API_K, next_token);
+         else
+            passes_left = 0;
+      }
+      while (passes_left > 0 && next_token.length() > 15 && loop > 19);
+      // do not more than 3 queries
+      // until 20 results received
+      // probability on next token exists
+      // all_places -> dB;
+      return true;
+   }
+
+   String readJStringFromFarFarAway(String distant_star){
+      String nextln, assembled_str = "";
+      try {
+         URL url = new URL(distant_star);
+         HttpURLConnection open_pipe = (HttpsURLConnection) url.openConnection();
+         if (open_pipe.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            Log.d("Get JSON","Https connection failed");
+            return "" + false;
+         }
+         InputStream in_str = open_pipe.getInputStream();
+         InputStreamReader in_reader = new InputStreamReader(in_str);
+         BufferedReader buf_reader = new BufferedReader(in_reader);
+
+         while ((nextln = buf_reader.readLine()) != null) {
+            if (!nextln.endsWith("\n")) nextln += "\n";
+            assembled_str += nextln;
+         }
+
+      } catch (IOException e) {
+         Log.d("Full20","Failed to combine URL");
+         return "" + false; // Aborted
+      }
+      return assembled_str;
+   }
+}
---
+++ app/src/main/java/javaghost/neiexplorer/MiningCave.java
@@ -0,0 +1,229 @@
+package javaghost.neiexplorer;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.location.Location;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by JavaGhost on 7/12/2016.
+ */
+public class MiningCave /*extends SQLiteOpenHelper*/ {
+
+   private static final int DATABASE_VERSION = 2;
+   private static OpenHelper mSQL = new OpenHelper();
+   SQLiteDatabase _db;
+   StatePillar    reflection;
+   ContentValues cv = new ContentValues();
+
+   static final String
+      TBL_PLACES     = "neixpl_places",
+      cl_pID         = "idp",             // VERSION MUST be CREATED if _id style NEEDED or just kill dB
+      cl_GGLID       = "place_id",
+      cl_LOC         = "location",
+      cl_NAME        = "nameof_place",
+      cl_VICI        = "vicinity",
+      cl_TYPICON     = "icon_uri",
+      cl_GGLURI      = "maps_uri",
+      cl_FLG_PH      = "has_photo",
+      crt_PLACES
+  = "CREATE TABLE %1$s (%2$s INTEGER PRIMARY KEY AUTOINCREMENT, %3$s TEXT NOT NULL unique, %4$s TEXT, %5$s TEXT, %6$s TEXT, %7$s TEXT, %8$s TEXT, %9$s INTEGER)",
+      TBL_CONTEXTS   = "neixpl_contexts",
+      cl_cID         = "idc",
+      cl_CENTER      = "focal_loc",
+      cl_TYPE        = "type_used",
+      cl_WORD        = "word_used",
+      cl_RADIUS      = "radius",
+      cl_CNT_HASH    = "hash",
+      crt_CONTEXTS
+  = "CREATE TABLE %1$s (%2$s INTEGER PRIMARY KEY AUTOINCREMENT, %3$s TEXT, %4$s TEXT, %5$s TEXT, %6$s INTEGER, %7$s INTEGER)", 
+      TBL_SYNTH      = "neixpl_synthviews",
+      cl_vID         = "idv",
+      cl_IDcs        = "idc",
+      cl_IDps        = "idp",
+      cl_FAVORED     = "favored",
+      cl_DISTANCE    = "focal_dist",
+      crt_SYNTH_FK
+  = "CREATE TABLE %1$s (%2$s INTEGER NOT NULL, %3$s INTEGER NOT NULL, %4$s INTEGER, %5$s INTEGER, " +
+     "FOREIGN KEY (%2$s) REFERENCES %6$s(%7$s), FOREIGN KEY (%3$s) REFERENCES %8$s(%9$s), PRIMARY KEY (%2$s, %3$s))",
+      crt_SYNTH
+  = "CREATE TABLE %1$s (%6$s INTEGER PRIMARY KEY AUTOINCREMENT, %2$s INTEGER NOT NULL, %3$s INTEGER NOT NULL, %4$s INTEGER, %5$s INTEGER)"
+   ;
+/*   CREATE TABLE table1 (
+           table2_id INTEGER NOT NULL,
+           table3_id INTEGER NOT NULL,
+           FOREIGN KEY (table2_id) REFERENCES table2(id),
+           FOREIGN KEY (table3_id) REFERENCES table3(id),
+           PRIMARY KEY (table2_id, table3_id)
+   );*/
+
+   private static class OpenHelper extends SQLiteOpenHelper {
+      private OpenHelper() {
+         super(StatePillar.host_context, StatePillar.host_context.getResources().getString(R.string.SQL_DB_NAME), null, DATABASE_VERSION);
+      }
+
+      @Override
+      public void onCreate(SQLiteDatabase db) {
+         String spell1 = String.format(crt_PLACES, TBL_PLACES, cl_pID, cl_GGLID, cl_LOC, cl_NAME, cl_VICI, cl_TYPICON, cl_GGLURI, cl_FLG_PH);
+         String spell2 = String.format(crt_CONTEXTS, TBL_CONTEXTS, cl_cID, cl_CENTER, cl_TYPE, cl_WORD, cl_RADIUS, cl_CNT_HASH);
+         String spell3 = String.format(crt_SYNTH_FK, TBL_SYNTH, cl_IDcs, cl_IDps, cl_FAVORED, cl_DISTANCE, TBL_CONTEXTS, cl_cID, TBL_PLACES, cl_pID);
+         String spell4 = String.format(crt_SYNTH, TBL_SYNTH, cl_IDcs, cl_IDps, cl_FAVORED, cl_DISTANCE, cl_vID);
+         db.execSQL(spell1);
+         db.execSQL(spell2);
+         db.execSQL(spell4);
+      }
+
+      @Override
+      public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+         db.execSQL("DROP TABLE IF EXISTS " + TBL_CONTEXTS);
+         db.execSQL("DROP TABLE IF EXISTS " + TBL_SYNTH);
+         db.execSQL("DROP TABLE IF EXISTS " + TBL_PLACES);
+         onCreate(db);
+      }
+
+      public static SQLiteDatabase getDatabase() { // pattern 1
+         return mSQL.getWritableDatabase();
+      }
+
+      public static int fetchData() { // pattern 2
+         SQLiteDatabase db = mSQL.getWritableDatabase();
+         // run some queries on the db and return result
+         return 1;
+      }
+   }
+
+   public MiningCave () {
+      reflection = StatePillar.getInstance();
+      _db = OpenHelper.getDatabase();
+      /*// TODO DEBUG
+      _db.execSQL("DROP TABLE IF EXISTS " + TBL_SYNTH);
+      _db.execSQL("DROP TABLE IF EXISTS " + TBL_CONTEXTS);*/
+   }
+   public long lastContext(){
+      long maxid = 0;
+      Cursor cursor = _db.query(TBL_CONTEXTS, new String[] {"MAX("+cl_cID+")"}, null, null, null, null, null);
+      if (cursor != null) {
+         cursor.moveToFirst();
+         maxid = cursor.getLong(0);
+         cursor.close();
+      }
+      return maxid;
+   }
+   //   public void updatePlaceURI(JustPlace for_place){   } -- done by upstore(JustPlace)
+   // polymorphic update for PLACES table
+   public long upstore(JustPlace that){
+      cv = new ContentValues();
+        cv.put(cl_GGLID, that._place_id);
+        cv.put(cl_LOC, ""+that._location.latitude+","+that._location.longitude);
+        cv.put(cl_NAME, that._name);
+        cv.put(cl_VICI, that._vicinity);
+        cv.put(cl_TYPICON, that._icon_uri);
+        cv.put(cl_GGLURI, that._google_uri);
+        cv.put(cl_FLG_PH, (that._has_photo)? 1 : 0 );
+      long rowID;
+      rowID = included(that);
+      if ( rowID > 0) {
+         if (that._place_id.length() > 0)
+            rowID = _db.update(TBL_PLACES, cv, cl_GGLID + " =?", new String[]{that._place_id});
+      // else -- no update needed, no insert possible ->> just return record number
+      // implicitly this avoids blind updates with loss of place_uri BUT outdated info could happen to stay in DB
+      }
+      else
+         rowID = _db.insert(TBL_PLACES, null, cv);
+      return rowID;
+   }
+   // polymorphic update for SYNTH table
+   public long upstore(JustPlace that, boolean is_favored){
+      cv = new ContentValues();
+      cv.put(cl_FAVORED, (is_favored)? 1 : 0);
+      long rowID = getContxt();
+      String cntxt_row = String.valueOf(rowID);          // TBL_CONTEXTS
+      String place_row = String.valueOf(upstore(that));  // TBL_PLACES
+      // by calling upstore() not inclided() I eliminate check on pl'existance, considering implicit included() call from upstore() this is ok-ok
+      return _db.update(TBL_SYNTH, cv, cl_IDcs + " =?" + " and " + cl_IDps + " =?", new String[]{cntxt_row, place_row});
+   }
+   public long included(JustPlace that) {
+      return included(that._place_id);
+   }
+   public long included(String place_ggl_id){
+      long got_id = 0;
+      Cursor c = _db.query(TBL_PLACES, new String[] {cl_pID},
+              cl_GGLID +" =?", new String[] {place_ggl_id},
+              null, null, null);
+      int found = c.getCount();
+      if (found > 0) {
+         c.moveToFirst();
+         got_id = c.getLong(0);
+         c.close();
+      }
+      return got_id;
+   }
+//   public void idx_placeContxt(String focal_, String type_, String word_, String radius_ ){}
+//   public void idx_placeContxt(LatLng focal_, String type_, String word_, double radius_ ){}
+   public long getContxt(/*StatePillar current_state*/){
+      long got_id = lastContext();
+      Cursor c = _db.query(TBL_CONTEXTS, new String[] {cl_cID},
+              cl_CNT_HASH +" =?", new String[] {reflection.hashStString()},
+              null, null, null);
+      if (c.getCount() >0) {
+         c.moveToFirst();
+         long goal = c.getLong(0);
+         c.close();
+         return goal;
+      } else
+      {
+      cv = new ContentValues();
+         cv.put(cl_CENTER, reflection.getCenterNow());
+         cv.put(cl_TYPE,   reflection.getTypeWriter());
+         cv.put(cl_WORD,   reflection.getWordOfBlake());
+         cv.put(cl_RADIUS, reflection.getLookupRadius());
+         cv.put(cl_CNT_HASH, reflection.hashStString());
+
+         return _db.insert(TBL_CONTEXTS, null, cv);
+      }
+   }
+
+   public long storeSynth(ArrayList<JustPlace> setof_places) {
+
+      int numrow = 0;
+      long lastxt = lastContext();
+      long contxt = getContxt();
+      float[] distances = new float[3];
+      if (lastxt == contxt) {
+         return 0; // no data were written as lookup context was previously queried;
+      } else {
+         _db.beginTransaction();
+         cv = new ContentValues();
+         try {
+            for (int idx = 0; idx < setof_places.size(); idx++) {
+               JustPlace idx_place = setof_places.get(idx);
+               cv.put(cl_vID, contxt);
+               cv.put(cl_IDps, upstore(idx_place));
+               cv.put(cl_FAVORED, false);
+               Location.distanceBetween(reflection._now.latitude, reflection._now.longitude, idx_place._location.latitude, idx_place._location.longitude, distances);
+               cv.put(cl_DISTANCE, distances[0]);
+               if (_db.insertWithOnConflict(TBL_SYNTH, null, cv, SQLiteDatabase.CONFLICT_IGNORE) != -1)
+                  numrow++;
+               cv = new ContentValues();
+            }
+            _db.setTransactionSuccessful();
+         } catch (Exception e) {
+            //end the transaction on error too when doing exception handling
+//            _db.endTransaction();
+            throw e;
+         } finally {
+            //end the transaction on no error
+            _db.endTransaction();
+            if (numrow < 60)
+               Log.d("DB.SYNTH", "Succeeded insertion on " + numrow + " records.");
+         }
+      }
+      return numrow;
+   }
+
+
+}
